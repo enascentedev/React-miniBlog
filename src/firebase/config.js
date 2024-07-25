16f386078e8f9cec5f3d9766ad079fe639e3619c
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-07-25 09:10:28 -0300

    adicionando conexão com firebase criando páginas about e home

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEtJgq57DuAPb78SxODlmkgXZCwzYrKlo",
+  authDomain: "miniblog-f959f.firebaseapp.com",
+  projectId: "miniblog-f959f",
+  storageBucket: "miniblog-f959f.appspot.com",
+  messagingSenderId: "124043246779",
+  appId: "1:124043246779:web:452189206f877143683d9e"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {db};
