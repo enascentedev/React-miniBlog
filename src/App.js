20f242f7c9f7e097ff514c6fff207bed8318c2fd
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-07-25 22:03:31 -0300

    import's e criação de rotas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import './App.css';
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
-  return (
-    <div className="App">
+	return (
+		<div className="App">
 			<BrowserRouter>
-			<div className='container'>
-				<Routes>
-					<Route path='/' element={<Home/>}/>
-					<Route path='/About' element={<About/>}/>
-
-				</Routes>
-			</div>
+				<Navbar />
+				<div className='container'>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/About' element={<About />} />
+						<Route
+                path="/login"
+                element={ <Login />}
+              />
+              <Route
+                path="/register"
+                element={ <Register /> }
+              />
+					</Routes>
+				</div>
+				<Footer />
 			</BrowserRouter>
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
