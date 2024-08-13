ecef12575ecbf9b17c2793e59dad7689b18db4e0
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-08-12 21:46:02 -0300

    alterações de layout login

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,32 @@
 import styles from "./Home.module.css";
 
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
+
 const Home = () => {
+	const[query,setQuery]= useState("");
+	const[posts]= useState([]);
+
+	const handleSubmit=(e)=>{
+		e.preventDefault();
+	}
 	return (
 		<div className={styles.home}>
-			<h1>Home</h1>
+			<h1>Veja nossos posts mais recentes</h1>
+			<form onSubmit={handleSubmit} className={styles.search}>
+				<input type="text" placeholder="ou busque por tags..." onChange={(e)=> setQuery(e.target.value)}></input>
+				<button></button>
+			</form>
+			<div>
+				<h1>Posts...</h1>
+				{posts && posts.length === 0 && (
+					<div className={styles.noposts}>
+						<p>não foram encontrados posts</p>
+						<Link to="/posts/create" className="btn">Criar primeiro post </Link>
+					</div>
+				)}
+			</div>
 		</div>
 	)
 }
